58466beb97ae620e2f3eda09ed1702a9b61af371
caitlinsanchez <66741810+caitlinsanchez@users.noreply.github.com>
2021-09-06 13:04:41 -0400

    Initial Commit

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,12 @@ namespace TheBlogProject.Models
 {
     public class Post
     {
+        public int Id { get; set; }
+        public int BlogId { get; set; }
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(75, ErrorMessage = "{0} must be at least {2} characters long and at most {1} characters.", MinimumLength = 2)]
+        public string Title { get; set; }
     }
 }
